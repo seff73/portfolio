c144b8ccc7644d8f6cd91c8d92d19434a67d1151
seff73 <seff73@gmail.com>
2022-10-29 01:54:35 -0400

    mobile menu updated

--- components/NavBar.js
+++ components/NavBar.js
@@ -138,6 +138,7 @@ export default function NavBar() {
                                 smooth={true}
                                 offset={50}
                                 duration={500}
+                                onClick={()=> setIsOpen(!isOpen)}
                                 className='cursor-pointer hover:bg-blue-600 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium'
                             >
                                 Projects
